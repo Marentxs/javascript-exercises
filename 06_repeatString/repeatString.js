7d913f97b5961f81f968214ff118604eacaf6d07
Julian <111262742+Marentxs@users.noreply.github.com>
2025-09-05 13:14:32 -0500

    Solve exercises 6 and 7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (a, b) {
+  if (b < 0) {
+    return "ERROR";
+  }
+  let result = "";
+  for (let i = 0; i < b; i++) {
+    result += a;
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
