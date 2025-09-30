43ddf35e97d5c4ef15f1686d3164d60ea2a7571f
Julian <111262742+Marentxs@users.noreply.github.com>
2025-09-30 14:00:01 -0500

    complete js exercise 12 calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let multiply = 1;
+  for (let i = 0; i < array.length; i++) {
+    multiply *= array[i];
+  }
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let array = [];
+  for (let i = 1; i <= num; i++) {
+    array.push(i);
+  }
+
+  let multiply = 1;
+  for (let i = 0; i < array.length; i++) {
+    multiply *= array[i];
+  }
+  return multiply;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
