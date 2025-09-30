2bc1b30ed85bd5c52a6f52b11401e5848bcb2a18
Julian <111262742+Marentxs@users.noreply.github.com>
2025-09-30 18:49:50 -0500

    complete js exercise 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function cleanString(str) {
+  const punctuation = ".,!?;:'\"-()[]{}";
+  let str1 = str
+    .split("")
+    .filter((ch) => ch !== " " && !punctuation.includes(ch))
+    .join("")
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .join("");
 
+  let str2 = str
+    .split("")
+    .filter((ch) => ch !== " " && !punctuation.includes(ch))
+    .join("")
+    .toLowerCase();
+
+  return str1 === str2;
 };
 
 // Do not edit below this line
