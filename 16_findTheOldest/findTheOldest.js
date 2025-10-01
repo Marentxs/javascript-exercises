ef6bdf3ca05d2a09c3fe35f25843b66e22862a2b
Julian <111262742+Marentxs@users.noreply.github.com>
2025-10-01 08:54:59 -0500

    complete js exercise 16 findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const current = new Date().getFullYear();
+  const ages = arr.map(
+    (item) => (item.yearOfDeath || current) - item.yearOfBirth
+  );
+  const oldest = Math.max(...ages);
 
+  return arr.find(
+    (item) => (item.yearOfDeath || current) - item.yearOfBirth === oldest
+  );
 };
 
 // Do not edit below this line
