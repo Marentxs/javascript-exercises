7d5cf74015654df6bdeb94e1a0344386633f0ef2
Julian <111262742+Marentxs@users.noreply.github.com>
2025-10-01 08:05:17 -0500

    complete js exercise 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+  const n = Number(input);
+  if (n < 0) return "OOPS";
+  if (n === 0) return 0;
+  if (n <= 2) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
+console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
